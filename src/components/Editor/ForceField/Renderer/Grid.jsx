69c29e9b24667182b510d7927b6e696c92239a1c
Michael Schieben <babe@rockitbaby.de>
2016-01-05 14:32:45 +0100

    WIP refactor renderer: grid based around center
    make core components independent of stage width
    should use https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Patterns
    for dots in off

--- src/components/Editor/ForceField/Renderer/Grid.jsx
+++ src/components/Editor/ForceField/Renderer/Grid.jsx
@@ -3,27 +3,39 @@ import ForceFieldDescriptor from '../../../../ForceFieldDescriptor';
 
 export default React.createClass({
 
+
   render: function() {
 
-    const offsetX = Math.floor(this.props.stageWidth / 2 - this.props.fieldSize / 2) % this.props.gridUnit;
-    const offsetY = Math.floor(this.props.stageHeight / 2 - this.props.fieldSize / 2) % this.props.gridUnit
+    const fieldSize = this.props.fieldSize;
+    const GU = this.props.gridUnit;
+    const origin = {x: this.props.stageWidth / 2, y: this.props.stageHeight / 2};
+    const dotsColor = this.props.skin.dots;
+    let circles = [];
 
-    var circles = []
-    for(var x = offsetX; x < this.props.stageWidth; x = x + this.props.gridUnit) {
-      for(var y = offsetY; y < this.props.stageHeight; y = y + this.props.gridUnit) {
-        const [normalizedX, normalizedY] = this.props.normalizeCoordinates(x, y);
-        var forceFieldDescriptor = new ForceFieldDescriptor(normalizedX, normalizedY);
-        var radius = 1;
-        if(forceFieldDescriptor.isCenter()) {
-          continue;
+    [{x: 1, y: 1}, {x: 1, y: -1}, {x: -1, y: -1}, {x: -1, y: 1}].forEach(function(quadrantCoefficient) {
+
+      for(let ix = 0; ix < 10; ix++) {
+        for(let iy = 0; iy < 10; iy++) {
+
+          let x = quadrantCoefficient.x * ix;
+          let y = quadrantCoefficient.y * iy;
+          let forceFieldDescriptor = new ForceFieldDescriptor(x / 10, y / 10);
+          let radius = 1;
+          if(forceFieldDescriptor.isCenter()) {
+            continue;
+          }
+          if(forceFieldDescriptor.isContext()) {
+            radius = 1.5;
+          }
+          const classNames = forceFieldDescriptor.getClassNames();
+          circles.push(<circle key={`${x},${y}`} className={classNames} cx={x * GU} cy={-y * GU} r={radius} stroke={dotsColor} ></circle>)
         }
-        if(forceFieldDescriptor.isContext()) {
-          radius = 1.5;
-        }
-        const classNames = forceFieldDescriptor.getClassNames();
-        circles.push(<circle key={`${x},${y}`} className={classNames} cx={x} cy={y} r={radius} stroke={this.props.skin.dots} ></circle>)
       }
-    }
-    return <g>{circles}</g>;
+
+    });
+
+    let transform = 'translate(' + origin.x + ',' + origin.y + ')';
+    return <g id="Grid-dots" transform={transform}>{circles}</g>;
   }
+
 });
